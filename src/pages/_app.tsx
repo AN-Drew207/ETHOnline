d4df3dae37d079f3ebf898a70a2cb9743065345a
Rcontre360 <rcontreraspimentel@gmail.com>
2022-09-04 13:27:53 -0400

    fucking redux and web3 wallet connections

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,25 @@
-import "styles/globals.css";
-import "styles/global-tailwind.css";
-import "styles/index.css";
 import AppWrapper from "components/AppWrapper";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import {Provider} from "react-redux";
+
+import Web3Provider from "components/Web3Provider";
+import {store} from "redux/store";
+import "styles/globals.css";
+import "styles/global-tailwind.css";
+import "styles/index.css";
 
 dayjs.extend(relativeTime);
 
-const App = ({ Component, pageProps }) => (
+const App = ({Component, pageProps}) => (
   <>
-    <AppWrapper>
-      <Component {...pageProps} />
-    </AppWrapper>
+    <Provider store={store}>
+      <Web3Provider>
+        <AppWrapper>
+          <Component {...pageProps} />
+        </AppWrapper>
+      </Web3Provider>
+    </Provider>
   </>
 );
 
