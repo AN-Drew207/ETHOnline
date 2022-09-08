c415c16ebad7a5062be1eb7206e9e494952ea3d3
Rcontre360 <rcontreraspimentel@gmail.com>
2022-09-08 19:56:21 -0400

    modified superfluid functions

--- contracts/test/TransferManager.ts
+++ contracts/test/TransferManager.ts
@@ -1,11 +1,29 @@
 import hre, {waffle} from "hardhat";
 
-import {superfluidFixture} from "./fixtures/superfluid";
+import {approveFlow, createFlow} from "@utils/superfluid";
+import {basicFixture} from "./fixtures";
 
 const {loadFixture} = waffle;
 
 describe.only("TransferManager", () => {
   it("Should deploy properly", async () => {
-    const {superfluid, signer, accounts} = await loadFixture(superfluidFixture);
+    const {superfluid, accounts, transferManager, superToken} = await loadFixture(basicFixture);
+
+    const res = await (
+      await createFlow(hre, {
+        superToken,
+        sender: accounts[0],
+        receiver: accounts[1].address,
+        superfluid,
+        flowRate: "10",
+      })
+    ).exec(accounts[0]);
+    console.log(res);
+    //await approveFlow(hre, {
+    //sender: accounts[0],
+    //receiver: transferManager.address,
+    //superToken,
+    //superfluid,
+    //});
   });
 });
