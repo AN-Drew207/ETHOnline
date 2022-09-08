e9ac76ee5316d88594ec416e78449d1fa3136055
ivaniuss <ivan.emi94@gmail.com>
2022-09-07 19:40:00 -0500

    ..

--- contracts/test/index.ts
+++ contracts/test/index.ts
@@ -2,24 +2,33 @@ import hre from "hardhat";
 import {FollowNFT__factory, LensHub__factory} from "../types/factories/@aave/lens-protocol/contracts/core";
 import {DataTypes} from "../types/@aave/lens-protocol/contracts/core/LensHub";
 import {lensFixture} from "./fixtures/lens";
+import {
+  deployContract,
+  getAddrs,
+  initEnv,
+  ProtocolState,
+  waitForTx,
+  ZERO_ADDRESS,
+} from '../utils/index';
 
 describe.only("test-module", () => {
   it("Should deploy and interact with lens", async () => {
     await lensFixture();
-    //const addrs = getAddrs();
-    //const lensHub = LensHub__factory.connect(addrs["lensHub proxy"], governance);
-    //await waitForTx(lensHub.setState(ProtocolState.Unpaused));
-    //await waitForTx(lensHub.whitelistProfileCreator(user.address, true));
-    //const inputStruct: DataTypes.CreateProfileDataStruct = {
-    //to: user.address,
-    //handle: "zer0dot",
-    //imageURI:
-    //"https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan",
-    //followModule: ZERO_ADDRESS,
-    //followModuleInitData: [],
-    //followNFTURI:
-    //"https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan",
-    //};
-    //await waitForTx(lensHub.connect(user).createProfile(inputStruct));
+    const [governance, , user] = await initEnv(hre);
+    const addrs = getAddrs();
+    const lensHub = LensHub__factory.connect(addrs["lensHub proxy"], governance);
+    await waitForTx(lensHub.setState(ProtocolState.Unpaused));
+    await waitForTx(lensHub.whitelistProfileCreator(user.address, true));
+    const inputStruct: DataTypes.CreateProfileDataStruct = {
+    to: user.address,
+    handle: "zer0dot",
+    imageURI:
+    "https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan",
+    followModule: ZERO_ADDRESS,
+    followModuleInitData: [],
+    followNFTURI:
+    "https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan",
+    };
+    await waitForTx(lensHub.connect(user).createProfile(inputStruct));
   });
 });
