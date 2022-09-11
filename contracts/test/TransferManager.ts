4e2de4131fbb7817fbed79d67225d334a0ffada5
Rcontre360 <rcontreraspimentel@gmail.com>
2022-09-11 19:45:34 -0400

    transfer manager create flow working

--- contracts/test/TransferManager.ts
+++ contracts/test/TransferManager.ts
@@ -1,13 +1,16 @@
-import hre, { waffle } from "hardhat";
+import {expect} from "chai";
+import hre, {waffle} from "hardhat";
 
-import { approveFlow, createFlow } from "@utils/superfluid";
-import { basicFixture } from "./fixtures";
+import {approveFlow, createFlow} from "@utils/superfluid";
+import {basicFixture} from "./fixtures";
 
-const { loadFixture } = waffle;
+const {loadFixture} = waffle;
 
 describe.only("TransferManager", () => {
   it("Should deploy properly", async () => {
-    const { superfluid, accounts, transferManager, superToken } = await loadFixture(basicFixture);
+    const {superfluid, accounts, transferManager, superToken} = await loadFixture(basicFixture);
+    const receiver = accounts[3];
+    const flowRate = "200";
 
     await approveFlow(hre, {
       sender: accounts[0],
@@ -15,5 +18,15 @@ describe.only("TransferManager", () => {
       superToken,
       superfluid,
     });
+    await transferManager.createFlow(superToken.address, accounts[0].address, receiver.address, flowRate);
+
+    const flowInfo = await superfluid.cfaV1.getFlow({
+      superToken: superToken.address,
+      sender: accounts[0].address,
+      receiver: receiver.address,
+      providerOrSigner: accounts[0],
+    });
+
+    expect(flowInfo.flowRate, "Incorrect flow rate").to.be.equal(flowRate);
   });
 });
--- contracts/test/fixtures/index.ts
+++ contracts/test/fixtures/index.ts
@@ -1,26 +1,25 @@
 import hre from "hardhat";
 
-import { deploy } from "@utils/contracts";
-import { createSuperToken, upgradeToken } from "@utils/superfluid";
-import { superfluidFixture } from "./superfluid";
-import { MockERC20 } from "@sctypes/index";
+import {deploy} from "@utils/contracts";
+import {createSuperToken, upgradeToken} from "@utils/superfluid";
+import {superfluidFixture} from "./superfluid";
+import {MockERC20, TransferManager} from "@sctypes/index";
 
-import { lensFixture } from "./lens";
+import {lensFixture} from "./lens";
 
 export const basicFixture = async () => {
-  const { superfluid } = await superfluidFixture();
+  const {superfluid} = await superfluidFixture();
   const accounts = await hre.ethers.getSigners();
   const token = <MockERC20>await deploy(hre, "MockERC20", accounts[0], []);
-  const superToken = await createSuperToken(hre, { token, superfluid, signer: accounts[0] });
+  const superToken = await createSuperToken(hre, {token, superfluid, signer: accounts[0]});
   const amount = hre.ethers.utils.parseEther("10000").toString();
-  const transferManager = await deploy(hre, "TransferManager", accounts[0], [
-    superToken.address,
-    superfluid.host.contract.address,
-  ]);
+  const transferManager = <TransferManager>(
+    await deploy(hre, "TransferManager", accounts[0], [superToken.address, superfluid.host.contract.address])
+  );
 
   await token.mint(accounts[0].address, amount);
   await token.approve(superToken.address, amount);
-  await upgradeToken({ superToken, amount, signer: accounts[0] });
+  await upgradeToken({superToken, amount, signer: accounts[0]});
 
   return {
     transferManager,
