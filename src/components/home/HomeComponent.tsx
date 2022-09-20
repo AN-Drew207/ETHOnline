68d331291e1b3f386c1fed4e7cf7d496acf34b6c
Rcontre360 <rcontreraspimentel@gmail.com>
2022-09-20 13:44:24 -0400

    random publications

--- src/components/home/HomeComponent.tsx
+++ src/components/home/HomeComponent.tsx
@@ -1,40 +1,34 @@
-import { SimplePostComponent } from "components/common/posts/post";
+import {SimplePostComponent} from "components/common/posts/post";
 import React, {useState} from "react";
-import { useWeb3React } from "@web3-react/core";
-import { useQuery } from "@apollo/client";
+import {useWeb3React} from "@web3-react/core";
+import {useQuery} from "@apollo/client";
 
 import useGetContract from "hooks/useGetContract";
 import useAuthClient from "hooks/useAuthClient";
-import { GET_PUBLICATIONS, CREATE_PROFILE } from "utils/graphql/queries";
-import { makeMutation } from "utils/graphql";
-import { Button } from "components/common/button";
-import { profile } from "console";
+import {CREATE_PROFILE, EXPLORE_PUBLICATIONS} from "utils/graphql/queries";
+import {makeMutation} from "utils/graphql";
+import {Button} from "components/common/button";
+import {profile} from "console";
 
 const HomeComponent = () => {
-  const { account } = useWeb3React();
+  const {account} = useWeb3React();
   const getContract = useGetContract();
   const client = useAuthClient();
-  const { data, loading, error } = useQuery(GET_PUBLICATIONS, {
+  const {data, loading, error} = useQuery(EXPLORE_PUBLICATIONS, {
     variables: {
       request: {
-        profileId: "0x02",
+        sortCriteria: "LATEST",
         publicationTypes: ["POST", "COMMENT", "MIRROR"],
-        limit: 10,
+        limit: 20,
       },
     },
   });
+  const publications = data ? data.explorePublications.items : [];
+  console.log({publications});
 
-  let publications = [];
-
-  if (!loading) {
-    publications = data.publications.items;
-  }
-
-  React.useEffect(() => {
-    console.log("MAKE MUTATION");
-    if (client && false)
-      //to avoid error
-      makeMutation({
+  const createProfile = async () => {
+    if (client)
+      await makeMutation({
         mutation: CREATE_PROFILE,
         variables: {
           request: {
@@ -45,14 +39,8 @@ const HomeComponent = () => {
           },
         },
         client,
-      })
-        .then((val) => {
-          console.log("SUCCESS", val);
-        })
-        .catch((err) => {
-          console.log("myErr", err);
-        });
-  }, [client]);
+      });
+  };
 
   const postsMock = [
     {
@@ -87,8 +75,7 @@ const HomeComponent = () => {
       name: "Carl",
       photo: "/images/profile2.png",
       address: "0x12Ee2c0Ca07F32a177eC4c07ea8574E183FdeaC4",
-      message:
-        "I'm excited for the merge! It is almost here, let's see how it goes!",
+      message: "I'm excited for the merge! It is almost here, let's see how it goes!",
       onLike: () => console.log("I like it"),
       onMessage: () => console.log("I message it"),
       liked: false,
@@ -113,53 +100,46 @@ const HomeComponent = () => {
   ];
   return (
     <div className="flex flex-col md:py-8 py-4 w-full">
-      <h1 className="text-primary text-3xl font-bold pb-8 w-full text-center">
-        Home
-      </h1>
+      <h1 className="text-primary text-3xl font-bold pb-8 w-full text-center">Home</h1>
       <div className="flex w-full gap-4">
         <div className="flex flex-col 2xl:w-2/3">
           <div className="flex flex-col gap-4 w-full">
-            {!loading ?
+            {!loading ? (
               publications.map(
                 ({
                   profile,
                   metadata,
-                  stats
+                  stats,
                   // video,
                 }) => (
                   <SimplePostComponent
-                    name={profile['name']}
-                    photo={profile['picture']['original']['url']}
-                    address={profile['id']}
-                    message={metadata['content']}
+                    name={profile["name"]}
+                    photo={profile["picture"] ? profile["picture"]["original"]["url"] : ""}
+                    address={profile["id"]}
+                    message={metadata["content"]}
                     onLike={() => null}
                     onMessage={() => null}
-                    liked={stats['totalAmountOfMirrors']}
-                    likes={stats['totalAmountOfCollects']}
-                    comments={stats['totalAmountOfComments']}
-                    image={metadata['media']}
+                    liked={stats["totalAmountOfMirrors"]}
+                    likes={stats["totalAmountOfCollects"]}
+                    comments={stats["totalAmountOfComments"]}
+                    image={metadata["media"]}
                   />
-                ),
-              ) : 
+                )
+              )
+            ) : (
               <p>Loading...</p>
-            }
+            )}
           </div>
         </div>
         <div className="2xl:flex hidden flex-col border border-gray-300 shadow-md h-min w-1/3 rounded-xl p-4 sticky top-24 gap-4">
-          <h2 className="font-bold text-xl text-center text-primary">
-            Suggestions
-          </h2>
+          <h2 className="font-bold text-xl text-center text-primary">Suggestions</h2>
           <div className="flex flex-col gap-3">
-            {suggestionsMock.map(({ photo, name, address }) => {
+            {suggestionsMock.map(({photo, name, address}) => {
               return (
                 <div className="flex border border-gray-300 shadow-md gap-2 max-w-[100%] w-full rounded-md items-center justify-between">
                   <div className="w-2/3 flex gap-2 truncate">
                     <div className="sm:w-14 w-12 p-2 shrink-0 rounded-full">
-                      <img
-                        src={photo}
-                        className="sm:w-10 sm:h-10 w-8 h-8 rounded-full"
-                        alt=""
-                      />
+                      <img src={photo} className="sm:w-10 sm:h-10 w-8 h-8 rounded-full" alt="" />
                     </div>
                     <div className="flex flex-col p-2 shrink-0 rounded-full truncate w-2/3">
                       <h3 className="font-bold text-md">{name}</h3>
@@ -167,11 +147,7 @@ const HomeComponent = () => {
                     </div>
                   </div>
                   <div className="p-2 shrink-0">
-                    <Button
-                      size="small"
-                      className="py-1 text-[14px]"
-                      decoration="fill"
-                    >
+                    <Button size="small" className="py-1 text-[14px]" decoration="fill">
                       Follow
                     </Button>
                   </div>
